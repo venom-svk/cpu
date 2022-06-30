7860b5fbec53c08628b257170fdb2a0d6ace17f5
venom-svk <83373816+venom-svk@users.noreply.github.com>
2022-07-01 00:35:54 +0200

    Update cpu_hack.sh

--- cpu_hack.sh
+++ cpu_hack.sh
@@ -4,7 +4,7 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
 cd xmrig-6.14.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TTr8oUg5BC3LPHVFjpgnMeMPpdpYdebAQn.God_Miner#ek61-6h9x -px--cpu 4
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TTr8oUg5BC3LPHVFjpgnMeMPpdpYdebAQn.XirenDiuz#ek61-6h9x -px--cpu 4
 while[1];do
 sleep3
 done
